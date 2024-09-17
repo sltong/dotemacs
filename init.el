e4d20f638f630a624e894fa4e7e58162c17d70d2
λαω <lambda.alpha.omega@proton.me>
2024-09-17 04:01:58 -0600

    clear initial scratch buffer message and set initial mode

--- init.el
+++ init.el
@@ -26,6 +26,10 @@
 (setq kept-old-versions 3) ; keep oldest 3 files
 (setq kept-new-versions 5) ; keep newest 5 files
 
+;; initial scratch buffer
+(setq initial-major-mode 'fundamental-mode)
+(setq initial-scratch-message nil)
+
 (setq column-number-mode t)
 
 ;; `use-package'
