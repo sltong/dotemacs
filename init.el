e4f50186b9b326ac5bfbc99e0b0f252054e9eec4
λαω <lambda.alpha.omega@proton.me>
2024-09-17 01:29:35 -0600

    enable desktop-save-mode for saving sessions

--- init.el
+++ init.el
@@ -21,6 +21,10 @@
 ;; `use-package'
 (setq use-package-always-ensure t)
 
+(use-package desktop
+  :init
+  (desktop-save-mode))
+
 (use-package savehist
   :custom
   (savehist-file (expand-file-name "minibuffer-history.el" user-emacs-var-directory))
