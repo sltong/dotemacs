ecbcbdbcf816a3bf4e9d6bd088519473167193df
λαω <lambda.alpha.omega@proton.me>
2024-09-17 00:28:52 -0600

    use "cons" to evaluate and get correct backup directory string

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (setq use-short-answers t) ;; make "yes-or-no-p" accept "y" and "n"
 
 ;; file backups
-(setq backup-directory-alist '(("." . (expand-file-name "backups" user-emacs-directory))))
+(setq backup-directory-alist (list (cons "." (expand-file-name "backups/" user-emacs-directory))))
 (setq version-control t) ; create multiple, numbered backups
 (setq delete-old-versions t) ; automatically delete excess backup files
 (setq kept-old-versions 3) ; keep oldest 3 files
