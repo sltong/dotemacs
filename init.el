75cafbbdd46a61cb20ccb936ae47cbdb5f6ca6b2
λαω <lambda.alpha.omega@proton.me>
2024-09-16 23:52:11 -0600

    set directory for all file backups

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
 (load custom-file)
 
 ;; file backups
+(setq backup-directory-alist '(("." . (expand-file-name "backups" user-emacs-directory))))
 (setq version-control t) ; create multiple, numbered backups
 (setq delete-old-versions t) ; automatically delete excess backup files
 (setq kept-old-versions 3) ; keep oldest 3 files
 (setq kept-new-versions 5) ; keep newest 5 files
-
