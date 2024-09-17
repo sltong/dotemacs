56092018ea02555913655dc16d383826bfbd6287
λαω <lambda.alpha.omega@proton.me>
2024-09-17 02:26:57 -0600

    set default font to JetBrains Mono NL

--- custom.el
+++ custom.el
@@ -9,4 +9,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "JetBrains Mono NL" :foundry "JB" :slant normal :weight regular :height 120 :width normal)))))
