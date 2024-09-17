5d8cf7fdb24976d0016ade33f123d1d948873a80
λαω <lambda.alpha.omega@proton.me>
2024-09-17 08:50:42 -0600

    store eln-cache in var user-emacs-directory

--- early-init.el
+++ early-init.el
@@ -1,5 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 
+;; store `eln-cache' in the "var" user-emacs-directory
+(when (and (fboundp 'startup-redirect-eln-cache)
+           (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
