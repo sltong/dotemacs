e6e38649c0b90fa14b6d64c38d0e6e1cb9a4197a
λαω <lambda.alpha.omega@proton.me>
2024-09-16 23:37:38 -0600

    add configurations for version-controlling file backups

--- init.el
+++ init.el
@@ -4,3 +4,10 @@
 
 (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file)
+
+;; file backups
+(setq version-control t) ; create multiple, numbered backups
+(setq delete-old-versions t) ; automatically delete excess backup files
+(setq kept-old-versions 3) ; keep oldest 3 files
+(setq kept-new-versions 5) ; keep newest 5 files
+
