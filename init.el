652b264bcf6860d0f90c68897b3110e9c6e64187
λαω <lambda.alpha.omega@proton.me>
2024-09-17 19:43:35 -0600

    multiline backup-directory-alist

--- init.el
+++ init.el
@@ -17,7 +17,8 @@
 (setq use-short-answers t) ; make "yes-or-no-p" accept "y" and "n"
 
 ;; file backups
-(setq backup-directory-alist (list (cons "." (expand-file-name "backups" user-emacs-var-directory))))
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backups" user-emacs-var-directory))))
 (setq version-control t) ; create multiple, numbered backups
 (setq delete-old-versions t) ; automatically delete excess backup files
 (setq kept-old-versions 3) ; keep oldest 3 files
