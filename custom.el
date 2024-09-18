82589bb1e41cdf11652ffc7403cf7ba28962b365
λαω <lambda.alpha.omega@proton.me>
2024-09-17 21:25:18 -0600

    remove package-selected-packages from custom.el

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tinted))
- '(package-selected-packages nil))
+ '(custom-enabled-themes '(modus-vivendi-tinted)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
