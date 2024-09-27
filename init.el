c2a16832773a505de3e09e94e998f75652596585
λαω <lambda.alpha.omega@proton.me>
2024-09-26 20:17:30 -0600

    set autorevert use-package defer priority to medium

--- init.el
+++ init.el
@@ -178,6 +178,7 @@
 ;; to prevent fetching them from repositories
 (use-package autorevert
   :ensure nil
+  :defer λαω-use-package-defer-priority-medium
   :config
   (global-auto-revert-mode)
   :diminish (auto-revert-mode))
