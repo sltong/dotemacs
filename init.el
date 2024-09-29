03cf512a25e1ac3d32589fcae8c5fa6bfca6c39d
λαω <lambda.alpha.omega@proton.me>
2024-09-28 20:29:39 -0600

    make corfu popup info faster and preview current candidate

--- init.el
+++ init.el
@@ -738,7 +738,8 @@ This function adds the `expreg--sentence' expansion function to
   (corfu-cycle t) ; enable cycling for `corfu-next/previous'
   (corfu-separator ?\s) ; orderless field separator
   (corfu-scroll-margin 3)
-  (corfu-popupinfo-delay '(1.25 . 0.9)))
+  (corfu-popupinfo-delay '(0.5 . 0.25))
+  (corfu-preview-current t))
 
 (use-package corfu-terminal
   :after corfu
